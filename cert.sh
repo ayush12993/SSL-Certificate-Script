3e81b7b3de549ed0081545c0081e873271d565b6
ayush12993 <36598788+ayush12993@users.noreply.github.com>
2022-11-13 11:59:10 +0530

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -4,13 +4,13 @@ sleep 2s
 echo "creating certificate for server"
 touch server-ext.cnf
 # 1. Generate CA's private key and self-signed certificate
-openssl req -x509 -newkey rsa:4096 -days 2000 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=IN/ST=MP/L=SHIVPURI/O=STDTOG/OU=Education/CN=*.studytogether.tech/emailAddress=ayush2202ksr@gmail.com"
+openssl req -x509 -newkey rsa:4096 -days 2000 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=yourcountry/ST=yourstate/L=yourcity/O=yourorganization/OU=Education/CN=*.yourdomain/emailAddress=youremailaddress"
 
 echo "CA's self-signed certificate"
 openssl x509 -in ca-cert.pem -noout -text
 
 # 2. Generate web server's private key and certificate signing request (CSR)
-openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=IN/ST=MP/L=SHIVPURI/O=STDTOG/OU=Education/CN=*.studytogether.tech/emailAddress=ayush2202ksr@gmail.com"
+openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=yourcountry/ST=yourstate/L=yourcity/O=yourorganization/OU=Education/CN=*.yourdomain/emailAddress=youremailaddress"
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
 openssl x509 -req -in server-req.pem -days 2000 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
